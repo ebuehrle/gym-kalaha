5dec783f345b5fdb88216b159c8f7fc0dd102fd7
ebuehrle <43623224+ebuehrle@users.noreply.github.com>
2021-03-21 21:36:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from setuptools import setup
-from setuptools import find_packages
 
 setup(
     name='gym_kalaha',
     version='0.0.1',
-    packages=find_packages(),
     install_requires=['gym']
 )
